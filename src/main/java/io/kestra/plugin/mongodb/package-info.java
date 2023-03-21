6d36ba80a87b66075e922b59340e90f7d3b6d937
Loïc Mathieu <loikeseke@gmail.com>
2023-03-21 14:33:54 +0100

    feat: plugin categories and icons

---
+++ src/main/java/io/kestra/plugin/mongodb/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for using the MongoDB NoSQL database.",
+    categories = PluginSubGroup.PluginCategory.DATABASE
+)
+package io.kestra.plugin.mongodb;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
