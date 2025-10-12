8c5ac2cce47f11bc118abaa9bc34c692c7fb0fe4
mady20 <165700212+mady20@users.noreply.github.com>
2025-10-12 21:01:02 +0530

    Update Bulk.java

--- src/main/java/io/kestra/plugin/mongodb/Bulk.java
+++ src/main/java/io/kestra/plugin/mongodb/Bulk.java
@@ -3,6 +3,7 @@ package io.kestra.plugin.mongodb;
 import com.mongodb.client.model.*;
 import io.kestra.core.models.annotations.Example;
 import io.kestra.core.models.annotations.Metric;
+import io.kestra.core.models.executions.metrics.Counter;
 import io.kestra.core.models.annotations.Plugin;
 import io.kestra.core.runners.RunContext;
 import io.swagger.v3.oas.annotations.media.Schema;
