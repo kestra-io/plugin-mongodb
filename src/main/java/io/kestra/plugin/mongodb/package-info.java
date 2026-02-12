7cdea21cbc8d3d49b7e92f4b6774146b6e9de077
François Delbrayelle <fdelbrayelle@kestra.io>
2026-02-12 11:31:04 +0100

    chore: merge package categories attribute

--- src/main/java/io/kestra/plugin/mongodb/package-info.java
+++ src/main/java/io/kestra/plugin/mongodb/package-info.java
@@ -1,7 +1,5 @@
 @PluginSubGroup(
-    description = "This sub-group of plugins contains tasks for using a MongoDB database.",
-    categories = PluginSubGroup.PluginCategory.DATABASE,
-    categories = {
+    description = "This sub-group of plugins contains tasks for using a MongoDB database.",categories = {
         PluginSubGroup.PluginCategory.DATA
     }
 )
