9eed72f644a3c8b6daad6c97715af805b96a665e
François Delbrayelle <fdelbrayelle@gmail.com>
2026-02-13 08:18:31 +0100

    chore: update plugin categories (#65)
    * chore: update plugin categories
    
    * chore: update kestraVersion to 1.2.3
    
    * chore: merge package categories attribute
    
    ---------
    
    Co-authored-by: ben8t <pimpaudben@gmail.com>
    Co-authored-by: François Delbrayelle <fdelbrayelle@kestra.io>
    Co-authored-by: Malay Dewangan <66718045+Malaydewangan09@users.noreply.github.com>

--- src/main/java/io/kestra/plugin/mongodb/package-info.java
+++ src/main/java/io/kestra/plugin/mongodb/package-info.java
@@ -1,6 +1,7 @@
 @PluginSubGroup(
-    description = "This sub-group of plugins contains tasks for using a MongoDB database.",
-    categories = PluginSubGroup.PluginCategory.DATABASE
+    description = "This sub-group of plugins contains tasks for using a MongoDB database.",categories = {
+        PluginSubGroup.PluginCategory.DATA
+    }
 )
 package io.kestra.plugin.mongodb;
 
