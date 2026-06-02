6bcd2f41f019bc62e5ff3c19b65dbc16ee48738d
Malay Dewangan <malaydewangan310@gmail.com>
2026-06-02 17:18:52 +0530

    refactor: replace deprecated tasks and filters

--- src/main/java/io/kestra/plugin/mongodb/Trigger.java
+++ src/main/java/io/kestra/plugin/mongodb/Trigger.java
@@ -43,7 +43,7 @@ import io.kestra.core.models.annotations.PluginProperty;
                     tasks:
                       - id: return
                         type: io.kestra.plugin.core.debug.Return
-                        format: "{{ json(taskrun.value) }}"
+                        format: "{{ fromJson(taskrun.value) }}"
 
                 triggers:
                   - id: watch
