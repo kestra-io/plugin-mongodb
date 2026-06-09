e2a38d9dacdf9b4b5fda32bdfb3b9e254be791d4
Malay Dewangan <malaydewangan310@gmail.com>
2026-06-09 14:57:13 +0530

    fix(load): avoid double JSON serialization per document

--- src/test/java/io/kestra/plugin/mongodb/LoadTest.java
+++ src/test/java/io/kestra/plugin/mongodb/LoadTest.java
@@ -7,10 +7,12 @@ import java.io.OutputStream;
 import java.net.URI;
 import java.util.Locale;
 
+import org.bson.Document;
 import org.bson.types.ObjectId;
 import org.junit.jupiter.api.Test;
 
 import com.google.common.collect.ImmutableMap;
+import com.mongodb.client.MongoClient;
 
 import io.kestra.core.junit.annotations.KestraTest;
 import io.kestra.core.models.property.Property;
@@ -67,5 +69,53 @@ class LoadTest extends MongoDbContainer {
         assertThat(runOutput.getInsertedCount(), is(100));
         assertThat(runContext.metrics().stream().filter(e -> e.getName().equals("requests.count")).findFirst().orElseThrow().getValue(), is(10D));
         assertThat(runContext.metrics().stream().filter(e -> e.getName().equals("records")).findFirst().orElseThrow().getValue(), is(100D));
+    }
+
+    @Test
+    void runWithIdKey() throws Exception {
+        RunContext runContext = runContextFactory.of();
+        String database = "ut_" + IdUtils.create().toLowerCase(Locale.ROOT);
+
+        File tempFile = File.createTempFile(this.getClass().getSimpleName().toLowerCase() + "_idkey_", ".trs");
+        OutputStream output = new FileOutputStream(tempFile);
+
+        ObjectId expectedId = new ObjectId();
+        FileSerde.write(
+            output, ImmutableMap.of(
+                "id", expectedId.toString(),
+                "name", "john"
+            )
+        );
+        URI uri = storageInterface.put(TenantService.MAIN_TENANT, null, URI.create("/" + IdUtils.create() + ".ion"), new FileInputStream(tempFile));
+
+        Load put = Load.builder()
+            .connection(
+                MongoDbConnection.builder()
+                    .uri(Property.ofValue(connectionUri))
+                    .build()
+            )
+            .database(Property.ofValue(database))
+            .collection(Property.ofValue("load_idkey"))
+            .from(Property.ofValue(uri.toString()))
+            .idKey(Property.ofValue("id"))
+            .build();
+
+        Load.Output runOutput = put.run(runContext);
+
+        assertThat(runOutput.getInsertedCount(), is(1));
+
+        // The id field must have become the document _id as a real ObjectId,
+        // and the source field must have been removed (removeIdKey defaults to true).
+        try (MongoClient client = getMongoClient()) {
+            Document stored = client.getDatabase(database)
+                .getCollection("load_idkey", Document.class)
+                .find()
+                .first();
+
+            assertThat(stored, is(org.hamcrest.Matchers.notNullValue()));
+            assertThat(stored.get("_id"), is(expectedId));
+            assertThat(stored.containsKey("id"), is(false));
+            assertThat(stored.getString("name"), is("john"));
+        }
     }
 }
